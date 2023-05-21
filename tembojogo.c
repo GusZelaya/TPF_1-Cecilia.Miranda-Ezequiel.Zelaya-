651e014ecdcf20dff16018e6d1e1c8e0db19c864
GusZelaya <126886415+GusZelaya@users.noreply.github.com>
2023-05-21 03:32:08 -0400

    Update tembojogo.c
    cosas por hacer:
    *terminar de hacer los defines para los casos entrar y salir de las ventanas por los botones 
    *terminar la función para cambiar de ventana 
    *ordenar las herramientas del gtk 
    
    si no entendes algo avisame

--- tembojogo.c
+++ tembojogo.c
@@ -1,1 +1,106 @@
-#include <caipi>
+//#include <caipi>
+#include <stdlib.h>
+#include <stdio.h>
+#include <gtk/gtk.h>
+
+#define direccion "/home/lp1-2023/eclipse_workspace/Hoppiti_interfaz_tpf/Debug/interfazHoppity.glade"
+#define inicioabando 0
+#define bandoajuego 1
+#define bandoainicio 2
+#define juegoabando 3
+#define inicioastats 4
+#define inicioaayuda 5
+
+
+//widgets de utilidad
+GtkBuilder * builder;
+
+//widgets de las ventanas
+
+//widgets de VentaAyuda
+GtkWidget * VentanaAyuda;
+GtkWidget * AyudaAtras;
+
+//widgets de VentanaBando
+GtkWidget * VentanaBando;
+GtkWidget * america;
+GtkWidget * usrr;
+GtkWidget * aleatrorio;
+GtkWidget * BancoAtras;
+GtkWidget * PlayerName;
+GtkWidget * empezar;
+
+//widgets de VentanaInicio
+GtkWidget * VentanaInicio;
+GtkWidget * pvm;
+GtkWidget * mvm;
+GtkWidget * stats;
+GtkWidget * InicioAyuda;
+
+//widgets de VentaJuego
+GtkWidget * VentanaJuego;
+GtkWidget * JuegoOpciones;
+GtkWidget * JuegoAyuda;
+GtkWidget * JuegoAtras;
+GtkWidget * LabeldeControl;
+GtkWidget * NOMBREJUGADOR;
+GtkWidget * TURNO;
+GtkWidget * PUNTOSPLAYER;
+GtkWidget * PUNTOSMAQUINA;
+
+
+//funcion que inicia el juego en Jugador vs maquina
+void CambiarVen (GtkWidget *event_box, GdkEventButton *event, gpointer data)
+{
+	switch((int)data)
+	{
+	case(inicioabando):
+	// Mostramos ventanas y escondemos otras
+	    gtk_widget_hide (VentanaInicio);
+	    gtk_widget_show_all (VentanaBando);
+	    break;
+	case(bandoajuego):
+		gtk_widget_hide (VentanaBando);
+		gtk_widget_show_all (VentanaJuego);
+		break;
+	}
+}
+
+
+int main (int argc, char *argv[])
+ {
+	guint ret;
+	GError *error = NULL;
+
+	gtk_init (&argc, &argv);
+
+	builder = gtk_builder_new();
+	ret = gtk_builder_add_from_file(builder, direccion, &error);
+	if (ret == 0)
+		{
+			g_print("Error en la función gtk_builder_add_from_file:\n%s", error->message);
+			return 1;
+		}
+
+	//asignación de ventanas
+	VentanaInicio = GTK_WIDGET(gtk_builder_get_object(builder,"VentanaInicio"));
+	VentanaAyuda  = GTK_WIDGET(gtk_builder_get_object(builder,"VentanaAyuda"));
+	VentanaBando  = GTK_WIDGET(gtk_builder_get_object(builder,"VentanaBando"));
+	VentanaJuego  = GTK_WIDGET(gtk_builder_get_object(builder,"VentaJuego"));
+	
+	//botones para ventana de inicio
+	pvm  		= GTK_WIDGET(gtk_builder_get_object(builder, "pvm"));
+	g_signal_connect (pvm,"button-press-event",G_CALLBACK(CambiarVen),inicioabando);
+	mvm         = GTK_WIDGET(gtk_builder_get_object (builder,"mvm"));
+	g_signal_connect (pvm,"button-press-event",G_CALLBACK(CambiarVen),inicioabando);
+	stats       = GTK_WIDGET(gtk_builder_get_object (builder,"stats"));
+	g_signal_connect (pvm,"button-press-event",G_CALLBACK(CambiarVen),inicioastats);
+	InicioAyuda = GTK_WIDGET(gtk_builder_get_object (builder,"InicioAyuda"));
+	g_signal_connect (pvm,"button-press-event",G_CALLBACK(CambiarVen),inicioaayuda);
+
+	gtk_widget_show_all (VentanaInicio);
+
+	gtk_main();
+
+    return 0;
+ }
