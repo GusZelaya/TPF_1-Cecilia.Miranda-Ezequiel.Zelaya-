e838459b679c4cdd56c2c3415b5dcdf2e452dc06
GusZelaya <126886415+GusZelaya@users.noreply.github.com>
2023-05-30 01:14:51 -0400

    Update tembojogo.c
    terminado el viaje entre ventanas
    falta:
    implementar el tablero del juego 
    crear los modos de juego
    crear la manera de indentificar la ubicacion de las piezas en el tablero
    comentar todo el codigo antes de que se me olvide como funciona

--- tembojogo.c
+++ tembojogo.c
@@ -1,21 +1,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <gtk/gtk.h>
+#include <time.h>
 
 //defines chetos
-#define direccion "/home/lp1-2023/eclipse_workspace/Hoppiti_interfaz_tpf/Debug/interfazHoppity.glade"
-#define inicioabando 0
+#define direccion "/home/lp1-2023/eclipse_workspace/Hoppiti_interfaz_tpf/src/interfazHoppitypurete.glade"
+#define inicioabando1 0
+#define inicioabando2 12
 #define bandoajuego 1
 #define bandoainicio 2
 #define juegoabando 3
 #define inicioastats 4
 #define inicioaayuda 5
 #define ayudaainicio 6
+#define usa 88
+#define rusia 99
+#define neutral 00
+#define pVm 1
+#define mVm 0
 
 
 //widgets de utilidad
 GtkBuilder * builder;
 gchar * filename;
+
+struct config{
+	gchar   UserSide;
+	gchar 	MachineSide;
+	gchar 	mod;
+	gchar 	username[20];
+};
+
+typedef struct config datosJuego;
+datosJuego Juego;
 
 //widgets de las ventanas
 
@@ -27,7 +44,7 @@ GtkWidget * AyudaAtras;
 GtkWidget * VentanaBando;
 GtkWidget * america;
 GtkWidget * usrr;
-GtkWidget * aleatrorio;
+GtkWidget * Aleatorio;
 GtkWidget * BandoAtras;
 GtkWidget * PlayerName;
 GtkWidget * Empezar;
@@ -41,15 +58,13 @@ GtkWidget * InicioAyuda;
 
 //widgets de VentaJuego
 GtkWidget * VentanaJuego;
-GtkWidget * JuegoOpciones;
-GtkWidget * JuegoAyuda;
 GtkWidget * JuegoAtras;
 GtkWidget * LabeldeControl;
 GtkWidget * NOMBREJUGADOR;
 GtkWidget * TURNO;
 GtkWidget * PUNTOSPLAYER;
 GtkWidget * PUNTOSMAQUINA;
-
+GtkWidget * GrillaJuego;
 
 //widgets de VentanaStats
 GtkWidget * VentanaStats;
@@ -95,6 +110,39 @@ char* copy_file_to_string(const char* filename) {
     return str;
 }
 
+void SetMode (int data)
+{
+	if(data==(int)pVm)
+	{
+		Juego.mod='P';
+	}else{
+		Juego.mod='M';
+	}
+}
+//
+void SetSide(GtkWidget event_box, GdkEventButton *event, gpointer data)
+{
+	if(data==(int)usa)
+	{
+		Juego.UserSide='n';
+		Juego.MachineSide='b';
+	}else if(data==(int)rusia){
+		Juego.UserSide='b';
+		Juego.MachineSide='n';
+	}else if(data==(int)neutral){
+		srand(time(NULL));
+		int ban=(rand()%2);
+		if (ban==1)
+		{
+			Juego.UserSide='b';
+			Juego.MachineSide='n';
+		}else{
+			Juego.UserSide='n';
+			Juego.MachineSide='b';
+		}
+	}
+}
+
 void guardar(GtkFileChooserButton *button, gpointer user_data)
 {
     GtkFileChooser *file_chooser = GTK_FILE_CHOOSER(button);
@@ -103,6 +151,7 @@ void guardar(GtkFileChooserButton *button, gpointer user_data)
     //vuelve a la pantalla anterior
     g_print("archivo: %s \n",filename);
 }
+
 //funcion para confirmar que quiere abrir el archivo seleccionado
 void mostrar(GtkWidget event_box, GdkEventButton *event, gpointer data)
 {
@@ -133,13 +182,20 @@ void CambiarVen (GtkWidget *event_box, GdkEventButton *event, gpointer data)
 {
 	switch((int)data)
 	{
-	case(inicioabando):
+	case(inicioabando1):
 	// Mostramos ventanas y escondemos otras
 	    gtk_widget_hide (VentanaInicio);
+		SetMode(pVm);
 	    gtk_widget_show_all (VentanaBando);
 	    break;
+	case(inicioabando2):
+		gtk_widget_hide (VentanaInicio);
+		SetMode(mVm);
+		gtk_widget_show_all (VentanaBando);
+		break;
 	case(bandoajuego):
 		gtk_widget_hide (VentanaBando);
+		guardar_texto();
 		gtk_widget_show_all (VentanaJuego);
 		break;
 	case(bandoainicio):
@@ -158,9 +214,18 @@ void CambiarVen (GtkWidget *event_box, GdkEventButton *event, gpointer data)
 		gtk_widget_hide (VentanaAyuda);
 		gtk_widget_show_all (VentanaInicio);
 		break;
+	case(juegoabando):
+		gtk_widget_hide (VentanaJuego);
+		gtk_widget_show_all (VentanaBando);
 	}
 }
 
+void guardar_texto()
+{
+    // Obtener el contenido del campo de entrada
+	strncpy(Juego.username, gtk_entry_get_text(GTK_ENTRY(PlayerName)), 19);
+	gtk_label_set_text(GTK_LABEL(NOMBREJUGADOR), Juego.username);
+}
 
 int main (int argc, char *argv[])
  {
@@ -176,27 +241,28 @@ int main (int argc, char *argv[])
 			g_print("Error en la función gtk_builder_add_from_file:\n%s", error->message);
 			return 1;
 		}
-
 	//asignación de ventanas
 	VentanaInicio = GTK_WIDGET(gtk_builder_get_object(builder,"VentanaInicio"));
 	VentanaAyuda  = GTK_WIDGET(gtk_builder_get_object(builder,"VentanaAyuda"));
 	VentanaBando  = GTK_WIDGET(gtk_builder_get_object(builder,"VentanaBando"));
-	VentanaJuego  = GTK_WIDGET(gtk_builder_get_object(builder,"VentaJuego"));
+	VentanaJuego  = GTK_WIDGET(gtk_builder_get_object(builder,"VentanaJuego"));
 	VentanaStats = GTK_WIDGET(gtk_builder_get_object(builder,"VentanaStats"));
 
 	//botones para ventana de inicio
 	pvm  		= GTK_WIDGET(gtk_builder_get_object(builder, "pvm"));
-	g_signal_connect (pvm,"button-press-event",G_CALLBACK(CambiarVen),inicioabando);
+	g_signal_connect (pvm,"button-press-event",G_CALLBACK(CambiarVen),inicioabando1);
 	mvm         = GTK_WIDGET(gtk_builder_get_object (builder,"mvm"));
-	g_signal_connect (mvm,"button-press-event",G_CALLBACK(CambiarVen),inicioabando);
+	g_signal_connect (mvm,"button-press-event",G_CALLBACK(CambiarVen),inicioabando2);
 	stats       = GTK_WIDGET(gtk_builder_get_object (builder,"stats"));
 	g_signal_connect (stats,"button-press-event",G_CALLBACK(CambiarVen),inicioastats);
 	InicioAyuda = GTK_WIDGET(gtk_builder_get_object (builder,"InicioAyuda"));
 	g_signal_connect (InicioAyuda,"button-press-event",G_CALLBACK(CambiarVen),inicioaayuda);
 
+
 	//botones de ventana ayuda
 	AyudaAtras  = GTK_WIDGET (gtk_builder_get_object(builder,"AyudaAtras"));
 	g_signal_connect (AyudaAtras,"button-press-event",G_CALLBACK(CambiarVen),ayudaainicio);
+
 
 	//botones de ventana bando
 	//botones de acceso
@@ -204,19 +270,38 @@ int main (int argc, char *argv[])
 	g_signal_connect (BandoAtras,"button-press-event",G_CALLBACK(CambiarVen),bandoainicio);
 	Empezar     = GTK_WIDGET(gtk_builder_get_object(builder,"Empezar"));
 	g_signal_connect (Empezar,"button-press-event",G_CALLBACK(CambiarVen),bandoajuego);
+
+	//botones de configuracion
+	america		= GTK_WIDGET(gtk_builder_get_object(builder,"america"));
+	g_signal_connect (america,"button-press-event",G_CALLBACK(SetSide),usa);
+	usrr		= GTK_WIDGET(gtk_builder_get_object(builder,"usrr"));
+	g_signal_connect (usrr,"button-press-event",G_CALLBACK(SetSide),usrr);
+	Aleatorio	= GTK_WIDGET(gtk_builder_get_object(builder,"Aleatorio"));
+	g_signal_connect (usrr,"button-press-event",G_CALLBACK(SetSide),neutral);
+
+
 	//label nombre del jugador
-	PlayerName	= GTL_WIDGET(gtk_builder_get_object(builder,"PlayerName"));
-	g_signal_connect (PlayerName,"insert-text",G_CALLBACK(GuardarNombre),NULL);
+	PlayerName	= GTK_WIDGET(gtk_builder_get_object(builder,"PlayerName"));
+	gtk_entry_set_max_length (GTK_ENTRY (PlayerName), 19);
 
 
+	//ventana juego
+	//botones
+	JuegoAtras	= GTK_WIDGET(gtk_builder_get_object(builder,"JuegoAtras"));
+	g_signal_connect (JuegoAtras,"button-press-event",G_CALLBACK(CambiarVen),juegoabando);
 
+	//labels
+	NOMBREJUGADOR= GTK_WIDGET(gtk_builder_get_object(builder,"NOMBREJUGADOR"));
+	LabeldeControl= GTK_WIDGET(gtk_builder_get_object(builder,"LabeldeControl"));
+	TURNO		= GTK_WIDGET(gtk_builder_get_object(builder,"TURNO"));
 
+	//Grilla
+	GrillaJuego = GTK_WIDGET(gtk_builder_get_object(builder,"GrillaJuego"));
+
+	//ventana estadisticas
 	ChooserStats= GTK_WIDGET(gtk_builder_get_object(builder, "ChooserStats"));
 	BotonStats  = GTK_WIDGET(gtk_builder_get_object (builder,"BotonStats"));
 	TextoStats	= GTK_WIDGET(gtk_builder_get_object(builder,"TextoStats"));
-
-	//utilidades
-	gtk_text_view_set_editable(GTK_TEXT_VIEW(TextoStats), FALSE);
 
 	//signals
 	gtk_text_view_set_editable(GTK_TEXT_VIEW(TextoStats), FALSE);
