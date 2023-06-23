d1257f517741aac8490aa2eb1720594f8a768f5b
GusZelaya <126886415+GusZelaya@users.noreply.github.com>
2023-06-23 10:58:47 -0400

    Update Globales.h

--- src/Globales.h
+++ src/Globales.h
@@ -17,11 +17,13 @@
 #include <stdbool.h>
 
 //defines chetos
-#define direccion "/home/lp1-2023/eclipse_workspace/Tembojogo/src/interfazHoppitypurete.glade"
-#define PuntoNegro "/home/lp1-2023/eclipse_workspace/Tembojogo/src/utilizades/PuntoNegro.png"
-#define PuntoRojo "/home/lp1-2023/eclipse_workspace/Tembojogo/src/utilizades/PuntoRojo.png"
-#define EspacioBlanco "/home/lp1-2023/eclipse_workspace/Tembojogo/src/utilizades/EspacioBlanco.jpeg"
-#define RutaEstadisticas "/home/lp1-2023/eclipse_workspace/Tembojogo/src/Estadisticas"
+
+#define direccion "src/UwU.glade"
+#define PuntoNegro "src/utilidades/Juego/PuntoNegro.png"
+#define PuntoRojo "src/utilidades/Juego/PuntoRojo.png"
+#define EspacioBlanco "src/utilidades/Juego/EspacioBlanco.png"
+#define RutaEstadisticas "src/Estadisticas"
+#define salir 96
 #define inicioabando1 0
 #define inicioabando2 12
 #define bandoajuego 1
@@ -30,6 +32,10 @@
 #define inicioastats 4
 #define inicioaayuda 5
 #define ayudaainicio 6
+#define ayudajuego 222
+#define repoabando 7
+#define selecrepo 8
+#define repoavisitante 9
 #define neutral 00
 #define rusia 88
 #define usa 99
@@ -48,25 +54,44 @@
 #define Al 66
 #define si 11
 #define no 22
+#define BLANCO 1
+#define PERSONA 1
+#define PC 0
+#define N 500
+#define M 100
+#define TRUE 1
+#define FALSE 0
+#define statsainicio 10
+#define MEN 55
 
-//utilidad
+#define ESCALAR_SALTOS 100
+#define ESCALAR_PREDIT 50
+
+//utilidad//
 GtkBuilder * builder;
 gchar * filename;
+gchar * RepositorioCompartido;
 gchar	EncabezadoEstadiscas[]={"                                    Estadisticas"};
 gchar	StrControlEstadisticas[70];
 gint 	i,j;
 FILE  * Estadisticas;
 gint	victoria=1;
+char 	turnoActual;
+int		colorMaquina;
+int		colorJugador;
 int		BanderaLugar[3]={0,0,0};
 char	CadGuardadoStats[100];
-
+char primeritaVez;
 char tablero [16][16];
+int TPuntajeMaquina[16][16];
+int TPuntajeUsuario[16][16];
 
 struct config{
 	gchar   UserSide;
 	gchar 	MachineSide;
 	gchar 	mod;
 	gchar 	username[20];
+	gchar	machinename[20];
 	gchar   UserTurn;
 	gchar   MachineTurn;
 };
@@ -81,12 +106,41 @@ typedef struct coord{
 coord pcActual;
 coord pcSiguiente;
 
+typedef struct aaa{
+	coord locuraGuardar[100];
+	unsigned int locuraLongitud;
+}aaa;
+
+aaa LOCURA;
+
 typedef struct estadisticasJugador
 {
 	int ganadas;
 	int perdidas;
 	int totales;
 }StructEstadisticas;
+
+typedef struct string{
+	char* array;
+	unsigned int charsTotales;
+	unsigned int charsUsados;
+}string;
+
+typedef struct siguiente{
+	coord coord[M];
+	char tableroFalso[16][16];
+	int contRuta;
+
+}siguiente;
+
+typedef struct jugada{
+	coord actual;
+	siguiente ruta[N];
+	int puntaje[N];
+	int contRuta;
+
+}salto;
+
 
 //widgets de las ventanas
 
@@ -112,6 +166,7 @@ GtkWidget * pvm;
 GtkWidget * mvm;
 GtkWidget * stats;
 GtkWidget * InicioAyuda;
+GtkWidget * SALIR;
 
 //widgets de salir
 GtkWidget * sii;//Edit
@@ -122,13 +177,17 @@ GtkWidget * VentanaJuego;
 GtkWidget * JuegoAtras;
 GtkWidget * LabeldeControl;
 GtkWidget * NOMBREJUGADOR;
-GtkWidget * TURNO;
+GtkWidget * NOMBREJUGADOR2;
 GtkWidget * PUNTOSPLAYER;
 GtkWidget * PUNTOSMAQUINA;
 GtkWidget * GridJuego;
 GtkWidget * EventBox;
 GtkWidget * VentanaSalir;
 GtkWidget * Juegoayuda;
+GtkWidget * JuegoLabelTurno;
+GtkWidget * JuegoEnter;
+GtkWidget * TURNO;
+
 
 //widgets de VentanaStats
 GtkWidget * VentanaStats;
@@ -136,6 +195,84 @@ GtkWidget * ChooserStats;
 GtkWidget * BotonStats;
 GtkWidget * TextoStats;
 
+//widgets de VentanaBusquedaPartida
+GtkWidget * VentanaBusquedaPartida;
+GtkWidget * BotonBusquedaPartida;
+GtkWidget * BotonOkBusquedaPartida;
+
+//widgets de VentanaVisitante
+GtkWidget * VentanaVisitante;
+GtkWidget * EntryVisitante;
+
+//widgets de VentanaMvM
+GtkWidget * VentanaMvM;
+GtkWidget * MvMLocal;
+GtkWidget * MvMVisitante;
+
+//widgets de VentanaVictoria
+GtkWidget * VentanaVictoria;
+GtkWidget * MENU;
+GtkWidget * LabelVictoria;
 
 
-#endif /* GLOBALES_H_ */
+
+//widgets imagenes
+
+//Victoria
+
+GtkWidget * FONDOVICTORIA;
+
+//inicio
+GtkWidget * PLAYERVSPC;
+GtkWidget * PCVSPC;
+GtkWidget * STATS;
+GtkWidget * HELPINICIO;
+GtkWidget * HOPPITY;
+GtkWidget * COLDWAR;
+GtkWidget * FONDOINICIO;
+GtkWidget * sal;
+
+//Bando
+GtkWidget * FONDOBANDO;
+GtkWidget * BACKBANDO;
+GtkWidget * RANDOM1;
+GtkWidget * RANDOM2;
+GtkWidget * START;
+GtkWidget * USA;
+GtkWidget * URRS;
+GtkWidget * PCJUEGO;
+GtkWidget * PLAYER;
+GtkWidget * PLAYERNAME;
+GtkWidget * CHOSESIDE;
+GtkWidget * INICIO;
+
+//Juego
+GtkWidget * BACKJUEGO;
+GtkWidget * HELPJUEGO;
+GtkWidget * PLAYER1;
+GtkWidget * PLAYER2;
+GtkWidget * TABLERO;
+GtkWidget * FICHAUSA;
+GtkWidget * FICHAURRS;
+
+//salir
+GtkWidget * SI;
+GtkWidget * NO;
+GtkWidget * SEGURO;
+
+//victoria
+GtkWidget * VentanaVictoria;
+GtkWidget * BACKWIN;
+
+//widgets de VentanaCreacionPartida
+GtkWidget * VentanaCreacionPartida;
+GtkWidget * BotonCreacionPartida;
+GtkWidget * BotonOKCreaciondePartida;
+
+//widgets de VentanaBusquedaPartida
+GtkWidget * VentanaBusquedaPartida;
+GtkWidget * BotonBusquedaPartida;
+GtkWidget * BotonOkBusquedaPartida;
+
+
+#endif /* GLOBALES_H_ */
